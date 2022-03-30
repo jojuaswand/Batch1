9e40092fc2ad035addb1b83b3f90b051a0ca2f9b
jojuaswand <jojuaswand10@gmail.com>
2022-03-30 12:51:56 +0530

    day1

---
+++ Basics/identifier_variables_keywords.py
@@ -0,0 +1,32 @@
+x = 10
+simran = 1
+y = 20
+print(x,y)
+"x".isidentifier()
+"simran".isidentifier()
+"_simran".isidentifier()
+"1simran".isidentifier()
+" simran".isidentifier()
+
+print(id(x))
+print(id(y))
+x = 5
+
+print(id(x))
+print(id(y))
+y = 5
+print(id(x))
+print(id(y))
+
+id(10)
+
+help("keywords")
+
+import keyword
+keyword.kwlist
+print(keyword.iskeyword("x"))
+print(keyword.iskeyword(x))
+print(keyword.iskeyword(y))
+print(keyword.iskeyword("true"))
+print(keyword.iskeyword("True"))
+
--- Basics/print_input.py
+++ Basics/print_input.py
@@ -1,14 +1,14 @@
 x = 10
 simran = 1
 y = 20
-# print(x)
-# print(y)
-# print(simran)
+print(x)
+print(y)
+print(simran)
 
-#print(x,y, sep=".", end="@") #default values sep = " " end = "\n"
-# print("hello", "world", sep= ",", end="/")
+print(x,y, sep=".", end="@") #default values sep = " " end = "\n"
+print("hello", "world", sep= ",", end="/")
 
-# x = input()
-# print(x,end=" #")
+x = input()
+print(x,end=" #")
 print(input("write the message here: "))
 
