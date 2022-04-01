33a89db1c2aed3812936934ce1636fe19e37d6cc
jojuaswand <jojuaswand10@gmail.com>
2022-04-01 12:36:07 +0530

    day2

--- Basics/print_input.py
+++ Basics/print_input.py
@@ -1,14 +1,23 @@
 x = 10
-simran = 1
+
 y = 20
-print(x)
-print(y)
-print(simran)
+# print(x)
+# print(y)
+# print(x,y,10,20)
+# print(x,y, sep=".", end="@") #default values sep = " " end = "\n"
+# print("hello", "world", sep=",", end="/")
+#
+# x = input()
+# print(x,end=" #")
+# print(input("write the message here: "))
 
-print(x,y, sep=".", end="@") #default values sep = " " end = "\n"
-print("hello", "world", sep= ",", end="/")
+x = "hi"
+y = "bye"
 
-x = input()
-print(x,end=" #")
-print(input("write the message here: "))
+# print("#", x, y, sep="&", end="#")
+
+# print("hi world", "bye world", sep="%", end="*")
+a = input("write your name: ")
+b = input("write your domain: ")
+print(a, b,end=".com",sep="@")
 
---
+++ individualdata/d1.py
@@ -0,0 +1,30 @@
+x = 1 #by default integer will be having 0
+# y = int(2)
+# print(type(x))
+# print(type(y))
+# print(type(-5))
+# print(x+y)
+# print(x-y)
+# print(x/y)
+# print(x*y)
+#
+# print(abs(x-y))
+# print(abs(-8))
+# print(abs(8))
+#
+# print(type(1.5)) #by default float will be having 0.0
+# print(type(1.))
+# print(1.)
+# print(type(-1.0))
+# print(abs(+1.12))
+# print(abs(-1.12))
+
+# print(round(2.45))
+# print(round(2.53))
+# print(round(2.45, 1))
+# print(round(2.44, 1))
+# print(round(2.3456, 2))
+# print(round(2.3456, 3))
+# print(round(2.3454, 3))
+# print(round(-2.3456, 3))
+
