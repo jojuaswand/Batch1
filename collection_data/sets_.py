d70affe32ccc45d05235b78a9d823ab19343d34c
jojuaswand <jojuaswand10@gmail.com>
2022-05-03 10:40:48 +0530

    set

---
+++ collection_data/sets_.py
@@ -0,0 +1,29 @@
+# s = set() # empty set is signified as set() itself
+# print(s, type(s)) # empty set is default value of set
+# s = set("hello")
+# print(s, type(s))
+# # set is unordered, and set doesnt take duplicates
+# # {} is the boundary for sets
+# s = {}
+# print(s, type(s)) # {} is empty dictionary not empty set
+# # {} is the boundary of set when there is a value
+# s = {1, 2}
+# print(s, type(s))
+
+# s = {1, 2, "hi"}
+# s.add(6)
+# s.add("a")
+# print(s)
+# # print(s[-1]) TypeError: 'set' object is not subscriptable
+# s.update("hello")
+# s.update([3, 4])
+# # s.add([7, 8])TypeError: unhashable type: 'list' # mutable data cannot be added to a set
+# print(s)
+#
+# # print(s.pop()) # pop removes a random element from the set
+# print(s.remove(2)) # removes a specified element
+# print(s)
+
+
+
+
